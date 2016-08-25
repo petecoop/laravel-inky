dae38907a251d9b2ad011c4dfa192c2528416094
petecoop <pete@petecoop.co.uk>
2016-08-25 22:26:42 +0100

    init

---
+++ src/Inky.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Petecoop\LaravelInky;
+
+
+class Inky
+{
+    
+}
---
+++ src/InkyServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Petecoop\LaravelInky;
+
+use Illuminate\Support\ServiceProvider;
+
+class InkyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
